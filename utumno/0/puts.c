b5d9b80474d02e4b8dbc5191871ccd2c108edd33
Elon Gliksberg <elongliks@gmail.com>
2020-03-15 17:26:26 +0200

    Solved Utumno0.

--- utumno/0/puts.c
+++ utumno/0/puts.c
@@ -1,23 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 
 int puts(const char *s) {
-	size_t nread;
-	FILE* exe;
-	char buf[4096];
-
-	exe = fopen("/proc/self/exe", "rb");
-	if (exe == NULL) {
-		printf("Failed to open file.");
-		return -1;
-	}
-	while ((nread = fread(buf, 1, sizeof(buf), exe)) > 0) {
-		fwrite(buf, 1, nread, stdout);
-	}
-	if (ferror(exe)) {
-		printf("An error has occured.");
-		return -1;
-	}
-	fclose(exe);
+	for (char* i = s - 0xff; i < s + 0xff; i++) { printf("%c", *i); }
 }
